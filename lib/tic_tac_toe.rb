082d97713f3c86a6a1d84789b86509359525f821
Igor Veyner <igorveyner95@gmail.com>
2020-05-20 17:51:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,6 +88,12 @@ class TicTacToe
     end
   end
   
-  
+  def over?(board)
+  if won?(board) != false || draw?(board) == true || full?(board) == true
+    true
+  else 
+    false
+  end
+end
   
 end
