7ea87f247367b4f6890e2926b130ab18c9d538c3
Igor Veyner <igorveyner95@gmail.com>
2020-05-20 17:48:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,9 +64,9 @@ class TicTacToe
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
     
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combo
